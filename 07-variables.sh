050600393a4d24cac7ddf339e1f1053a9d49b4de
Manoj Kumar Nadipelli <manojn031993@gmail.com>
2026-04-29 21:17:44 +0100

    actions

--- 07-variables.sh
+++ 07-variables.sh
@@ -2,11 +2,11 @@
 
 ### Special Variables ###
 echo "All args passed to script: $@"
-echo "Number of vars passed to script: $#
+echo "Number of vars passed to script: $#"
 echo "script name: $0"
 echo "Present Directory: $PWD"
 echo "Who is running: $USER"
-echo "Home directory of current user: $Home"
+echo "Home directory of current user: $HOME"
 echo "PID of the script: $$"
 sleep 100 &
 echo "PID of recently executed background process: $!"
