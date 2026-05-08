635d9ccb0b1800e80e9c1fa578311078af9b745a
Manoj Kumar Nadipelli <manojn031993@gmail.com>
2026-05-08 21:25:37 +0100

    actions

--- 15-loops.sh
+++ 15-loops.sh
@@ -22,9 +22,9 @@ VALIDATE(){
 
 for package in $@ # sudo sh 14-loops.sh nginx mysql nodejs
 do
-    dnf installed $package -y &>> $LOGS_FILE
+    dnf list installed $package &>> $LOGS_FILE
     if [ $? -ne 0 ]; then
-        echo "$package installed, installing now"
+        echo "$package not installed, installing now"
         dnf install $package -y &>> $LOGS_FILE
         VALIDATE $? "$package installation"
     else
