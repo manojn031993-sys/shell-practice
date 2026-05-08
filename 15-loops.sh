ea42e020b8057d76913d6c3a5c9602892b2c10bc
Manoj Kumar Nadipelli <manojn031993@gmail.com>
2026-05-08 21:13:33 +0100

    actions

---
+++ 15-loops.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/12-logs.log"
+
+if [ $USERID -ne 0 ]; then
+    echo "please run this script as root user access or sudo access" | tee -a $LOGS_FILE
+    exit 1
+fi    
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo "$2 ... FAILURE" | tee -a $LOGS_FILE
+        exit 1
+    else 
+        echo "$2 ... SUCCESS" | tee -a $LOGS_FILE
+    fi
+}
+
+for package in $@ # sudo sh 14-loops.sh nginx mysql nodejs
+do
+    dnf installed $package -y &>> $LOGS_FILE
+    if [ $? -ne 0 ]; then
+        echo "$package installed, installing now"
+        dnf install $package -y &>> $LOGS_FILE
+        VALIDATE $? "$package installation"
+    else
+        echo "$package is already installed, skipping installation"
+    fi
+    
+done
+
+
+
+
+
