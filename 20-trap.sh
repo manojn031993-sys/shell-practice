cb7158e1ecc5a95aa049dbdc586c1a375a4b2a4d
Manoj Kumar Nadipelli <manojn031993@gmail.com>
2026-05-10 12:04:15 +0100

    actions

---
+++ 20-trap.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+N="\e[0m"
+
+set -e
+trap 'echo "There is an error in $LINENO, Commanad: $BASH_COMMAND"' ERR
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/12-logs.log"
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R please run this script as root user access or sudo access $N" | tee -a $LOGS_FILE
+    exit 1
+fi    
+
+mkdir -p $LOGS_FOLDER
+
+for package in $@ # sudo sh 14-loops.sh nginx mysql nodejs
+do
+    dnf list installed $package &>> $LOGS_FILE
+    if [ $? -ne 0 ]; then
+        echo "$package not installed, installing now"
+        dnf install $package -y &>> $LOGS_FILE
+       
+    else
+        echo -e "$package is already installed...$Y skipping $N"
+        
+    fi
+done
